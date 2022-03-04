6676f2f8042e4c91f4e4dc57efbff4ea4177d929
FraSharp <s23265@iisve.it>
2022-03-04 20:04:58 +0100

    PRAWTB: index: cleanup, move some parameters to an external file, add comments
    Signed-off-by: FraSharp <s23265@iisve.it>

--- src/index.php
+++ src/index.php
@@ -4,28 +4,36 @@ require("vendor/autoload.php");
 require("config.php");
 use skrtdev\NovaGram\Bot;
 use skrtdev\NovaGram\CurlException;
-use skrtdev\Telegram\Message;
+
+/*
+ * this function can be used to dump json body into a text file called "dump.txt", currently you get the last post
+ * from r/frareddittest
+ */
+/**
+ * @throws JsonException
+ */
+function DumpJSON(): void {
+    $data = json_decode(file_get_contents("https://www.reddit.com/r/frareddittest/new.json?sort=new&limit=1"),
+        true, 512, JSON_THROW_ON_ERROR);
+    file_put_contents("dump.txt", print_r($data, true));
+}
 
 try {
     $Bot = new Bot(getenv("token"), [
-        "command_prefixes" => [':'],
         "skip_old_updates" => true,
-        "threshold" => 50,
         "parse_mode" => "HTML"
     ]);
 } catch (CurlException|\skrtdev\NovaGram\Exception $e) {
 }
 
-$Bot->onChannelPost(function (Message $message) use ($Bot) {
-    $a = new SubReddit("frareddittest", $Bot, $message->chat->id);
-    $a->SetSubRedditPostsLimit(1);
-    if (!$a->sendPost()) {
-        echo "\nPost already sent";
-    }
-
-});
+// CHANNEL_ID is defined in config.php
+$a = new SubReddit(SUBREDDIT_NAME, !isset($Bot) ?: $Bot, CHANNEL_ID);
+// set the limit of posts to retrieve
+$a->SetSubRedditPostsLimit(1);
+// check for new posts and if so, send them
+$a->Poll(30);
 
 try {
-    $Bot->start();
+    !isset($Bot) ?: $Bot->start();
 } catch (\skrtdev\NovaGram\Exception $e) {
 }
