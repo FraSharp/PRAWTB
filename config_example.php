70282c6ef17795f3fe6a33121c74cabef87960b3
FraSharp <s23265@iisve.it>
2022-03-04 20:06:21 +0100

    PRAWTB: config: add file with every config
    Signed-off-by: FraSharp <s23265@iisve.it>

---
+++ config_example.php
@@ -0,0 +1,7 @@
+<?php
+// put token after the =
+putenv("token=");
+// replace -1234567890 with the channel id
+const CHANNEL_ID = -1234567890;
+// replace "subreddit" with the subreddit you want to track
+const SUBREDDIT_NAME = "subreddit";
